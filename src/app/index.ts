7118f2edc84b581f81157cd0d4658c24735ba08f
Marvin Yang <marvinyangs@gmail.com>
2025-03-05 01:18:28 -0500

    refactor: do separate app.use instead of all in one

--- src/app/index.ts
+++ src/app/index.ts
@@ -4,10 +4,12 @@ import { authRouter } from "./routes/auth";
 import { ingredientsRouter } from "./routes/ingredients";
 import { recipesRouter } from "./routes/recipes";
 
-export const app = express().use(
-  express.json(),
-  authRouter,
-  ingredientsRouter,
-  recipesRouter,
-  errorHandler
-);
+export const app = express();
+
+app.use(express.json());
+
+app.use(authRouter);
+app.use(ingredientsRouter);
+app.use(recipesRouter);
+
+app.use(errorHandler);

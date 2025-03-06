5d60bc346352a5def049fdec9ac774ba00c94819
Marvin Yang <marvinyangs@gmail.com>
2025-03-06 00:33:35 -0500

    feat: handle cors

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,3 +1,5 @@
+import { config } from "@src/config";
+import cors from "cors";
 import express from "express";
 import { errorHandler } from "./middleware/error-handler";
 import { authRouter } from "./routes/auth";
@@ -7,6 +9,11 @@ import { recipesRouter } from "./routes/recipes";
 export const app = express();
 
 app.use(express.json());
+app.use(
+  cors({
+    origin: config.corsOrigin,
+  })
+);
 
 app.use(authRouter);
 app.use(ingredientsRouter);
--- src/config/config.dev.ts
+++ src/config/config.dev.ts
@@ -1,3 +1,4 @@
 export const devConfig = {
+  corsOrigin: process.env.CORS_ORIGIN?.split(",") || ["http://localhost:4200"],
   port: process.env.PORT || "3000",
 };
